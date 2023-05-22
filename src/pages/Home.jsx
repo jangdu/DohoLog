cd4132f46e3a4bb15fb35f35b30b4ed0e1c62cd4
jangdu <jjd0324@gmail.com>
2023-05-22 10:02:16 +0900

    fix: Posts컴포넌트 Home에 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Profile from "../components/Profile";
+import Posts from "../components/Posts";
 
 export default function Home() {
   return (
     <div>
       <Profile />
+      <Posts />
     </div>
   );
 }
