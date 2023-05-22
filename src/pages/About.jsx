bc12389daad53137df59c408004e21e6f7d8d292
jangdu <jjd0324@gmail.com>
2023-05-22 09:58:58 +0900

    add: Posts Component in mainPage

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Profile from "../components/Profile";
+import Posts from "../components/Posts";
 
 export default function About() {
   return (
     <div>
       <Profile />
+      <Posts />
     </div>
   );
 }
