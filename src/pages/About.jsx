429096a99f8ef41f5489aa568faadbcf167d55af
jangdu <jjd0324@gmail.com>
2023-05-22 21:50:06 +0900

    Fix: About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Profile from "../components/Profile";
-import Posts from "../components/Posts";
 
 export default function About() {
   return (
     <div>
       <Profile />
-      <Posts />
     </div>
   );
 }
