c73881dd8eb6ff0e042ea97f3b5f5ff2a8cb3ea7
jangdu <jjd0324@gmail.com>
2023-06-02 19:44:52 +0900

    feat: notionApi in about.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Profile from "../components/Profile";
+import GridNotion from "../components/GridNotion";
 
 export default function About() {
   return (
     <div>
       <Profile />
+      <GridNotion />
     </div>
   );
 }
